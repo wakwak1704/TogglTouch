634808afd1d1efec5fc4c5d33faadf32f7d7ef35
wakwak1704 <ryoya.second@gmail.com>
2020-12-24 23:20:48 +0900

    revert today's source code to modify email address of commit

--- src/com/kamijima/HelloServlet.java
+++ src/com/kamijima/HelloServlet.java
@@ -1,8 +1,6 @@
 package com.kamijima;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,40 +11,30 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class HelloServlet
  */
 @WebServlet("/HelloServlet")
-public class HelloServlet extends HttpServlet
-{
+public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public HelloServlet()
-	{
-		super();
-		// TODO Auto-generated constructor stub
-	}
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException
-	{
-		request.setAttribute("color", "light-blue");
-		request.setAttribute("message", "Input your favorite color.");
-		String view = "/WEB-INF/view/index.jsp";
-		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
-		dispatcher.forward(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException
-	{
-		String value = request.getParameter("hoge");
-		System.out.println(value);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
